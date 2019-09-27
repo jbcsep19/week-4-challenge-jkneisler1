c86618313d95beefc4444d297fdb882c4a0208ad
jkneisler1 <John.Kneisler@gmail.com>
2019-09-26 22:19:20 -0400

    Fifth submission

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 
 import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,4 +28,11 @@ public class HomeController {
     public String BootCampDescription() {
         return "bootCampDescription";
     }
+
+    @RequestMapping("/entireBlog")
+    public String EntireBlog() { return "entireBlog"; }
+
+    @RequestMapping("/shortBlog")
+    public String ShortBlog() { return "shortBlog"; }
+
 }
